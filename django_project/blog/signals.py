94a914e33207e8aef96f3309b4afb5cf8e5ad768
SatyamKumar02 <satyamksingh02@gmail.com>
2025-05-11 01:21:09 +0530

    added notification on new post

--- django_project/blog/signals.py
+++ django_project/blog/signals.py
@@ -1,8 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Comment
+from .models import Comment, Post
 from notifications.models import Notification
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 @receiver(post_save, sender=Comment)
 def notify_on_comment(sender, instance, created, **kwargs):
@@ -15,4 +18,18 @@ def notify_on_comment(sender, instance, created, **kwargs):
                 sender=instance.author,
                 verb=f"{instance.author.username} commented on your post: '{post.title}'",
                 url=notification_url
+            )
+
+@receiver(post_save, sender=Post)
+def notify_users_on_new_post(sender, instance, created, **kwargs):
+    if created:
+        author = instance.author
+        # Notify all users except the one who posted
+        recipients = User.objects.exclude(id=instance.author.id)
+        for user in recipients:
+            Notification.objects.create(
+                sender=author,
+                recipient=user,
+                verb=f"{instance.author.username} posted a new blog: {instance.title}",
+                url=reverse('blog:post-detail', args=[instance.pk])
             )
